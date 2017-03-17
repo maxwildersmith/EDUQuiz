2f0d95c9927150d51debfdfb50ab078979412c0e
max <maxwildersmith@gmail.com>
2017-03-17 12:34:08 -0700

    sdfg

--- Quiz.java
+++ Quiz.java
@@ -6,10 +6,11 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -24,10 +25,16 @@ import java.util.concurrent.ExecutionException;
 public class Quiz extends AppCompatActivity {
     private TextView title, timer,question;
     private RadioButton[] options;//opt1,opt2,opt3,opt4;
+    private Button submit;
     private CountDownTimer time;
     private JSONObject data;
-    private String API = "/api/v1/Articles/List?expand=1";
     public static final String DATA_NAME = "data stuffs";
+    private static final String GET_ARTICLES = "/api/v1/Articles/List?";
+    private static final String GET_INFO = "/api/v1/Articles/AsSimpleJson?id=";
+    private int correctopt;
+    private boolean started = false;
+
+    private String[] questions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,9 @@ public class Quiz extends AppCompatActivity {
         title = (TextView)findViewById(R.id.title);
         timer = (TextView)findViewById(R.id.timer);
         question = (TextView)findViewById(R.id.question);
+        submit = (Button)findViewById(R.id.next);
+
+        questions = new String[5];
 
         options = new RadioButton[4];
         options[0] = (RadioButton)findViewById(R.id.opt1);
@@ -44,8 +54,9 @@ public class Quiz extends AppCompatActivity {
         options[3] = (RadioButton)findViewById(R.id.opt4);
 
 
-        question.setText("Start!");
-        question.setOnClickListener(new View.OnClickListener() {
+
+        submit.setText("Start!");
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getData();
@@ -56,21 +67,60 @@ public class Quiz extends AppCompatActivity {
     }
 
     private void getData() {
+        if(started)
+            checkAnswer();
         try {
-            String s = new PersonSearch().execute("http://"+getIntent().getStringExtra(DATA_NAME)+API,null,"").get();
+            String s = new PersonSearch().execute("http://"+getIntent().getStringExtra(DATA_NAME)+GET_ARTICLES,null,"").get();
             try {
-                 data = new JSONObject(s);
-                Toast.makeText(Quiz.this, data.getJSONArray("items").getJSONObject(0).getString("title"), Toast.LENGTH_SHORT).show();
+                JSONArray articleList = new JSONObject(s).getJSONArray("items");
+                Log.e("asdf",articleList.length()+"");
+                JSONObject[] articles = new JSONObject[articleList.length()];
+                for(int i=0;i<articles.length;i++)
+                    articles[i] = articleList.getJSONObject(i);
+                int art = (int)(Math.random()*articles.length);
+                int correctOpt = (int)(Math.random()*4);
+                int x=-1;
+                for(int i=0;i<options.length;i++)
+                    if((x=(int)(Math.random()*articles.length))!=art)
+                        for(RadioButton r: options)
+                            if(r.getText().equals(articles[x].getString("title")))
+                                break;
+                            else
+                                options[i].setText(articles[x].getString("title"));
+                    else
+                        i--;
+                options[correctOpt].setText(articles[art].getString("title"));
+                JSONArray body = new JSONObject(new PersonSearch().execute("http://"+getIntent().getStringExtra(DATA_NAME)+GET_INFO+articles[art].getString("id"),null,"").get()).getJSONArray("sections");
+                JSONArray content = null;
+                question.setText("");
+                for(int i=0;i<body.length();i++)
+                    if((content = body.getJSONObject(i).getJSONArray("content")).length()!=0){
+                        String restofarticle = content.getJSONObject(0).getString("text").substring(content.getJSONObject(0).getString("text").indexOf("."));
+                        question.setText(restofarticle.substring(0,restofarticle.indexOf(".")+1));
+                    }
+                if(question.getText().equals(""))
+                    question.setText("No valid articles");
+
+                //question.setText(new PersonSearch().execute("http://"+getIntent().getStringExtra(DATA_NAME)+GET_INFO+articles[art].getString("id"),null,"").get());
+
             } catch (JSONException e) {
                 e.printStackTrace();
                 Log.e("asdf",""+s);
             }
 
+
+            started=true;
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+        submit.setText("Next");
+
+    }
+
+    private void checkAnswer() {
+
     }
 
     public class PersonSearch extends AsyncTask<String, Void, String> {
